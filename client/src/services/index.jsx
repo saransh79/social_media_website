29b34b25f56bc68d189f977f3228a565f92a457e
Saransh Gangwar <saransh7919@gmail.com>
2024-01-24 16:52:51 +0530

    updated

--- client/src/services/index.jsx
+++ client/src/services/index.jsx
@@ -1,3 +1,5 @@
-export const BASE_URL= 'https://social-media-website-8alg.vercel.app'
+// export const BASE_URL= 'https://social-media-website-8alg.vercel.app'
 
 // export const BASE_URL = "http://localhost:8000"
+
+export const BASE_URL= 'https://funstagram.onrender.com'
