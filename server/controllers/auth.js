8f9ae38b60fb0de8301fb973ae7e5b9d6159c0c9
Saransh Gangwar <saransh7919@gmail.com>
2024-01-24 15:10:10 +0530

    updated backend

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -11,11 +11,11 @@ export const register = async (req, res) => {
       email,
       password,
       picturePath,
-      // friends,
+      friends,
       location,
       occupation,
     } = req.body;
-
+    
     const salt = await bcrypt.genSalt();
     const passwordHash = await bcrypt.hash(password, salt);
 
@@ -25,7 +25,7 @@ export const register = async (req, res) => {
       email,
       password: passwordHash,
       picturePath,
-      // friends,
+      friends,
       location,
       occupation,
       viewedProfile: Math.floor(Math.random() * 10000)
